87df3bffd13c4765123dc90e7a90b57ac4008784
Tenzin <tenthinten@gmail.com>
2022-01-20 15:50:25 +0530

    display product

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import ProductComponent from './ProductComponent';
 
 const ProductListing = () => {
     const products = useSelector((state) => state)
     console.log(products)
     return (
         <div className='ui grid container'>
-            <h1>ProductListing</h1>
+           <ProductComponent/>
         </div>
     )
 }
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,7 +1,7 @@
 import { ActionTypes } from "../constants/action-types";
 
 const initialState = {
-    product: [
+    products: [
         {
         id:1,
         title:"Tenzin",
