537fe0b0ab7d29a088457d6d86ae512c69e71303
Tenzin <tenthinten@gmail.com>
2022-01-20 21:11:58 +0530

    dispatch selected product detail

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -2,14 +2,14 @@ import React,{ useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
-import  { selectedProducts } from '../reduc/actions/productActions'
+import  { selectedProducts } from '../redux/actions/productActions'
 import { useSelector } from 'react-redux';
 
 const ProductDetail = () => {
     const product = useSelector((state) => state.product);
     const {productId} = useParams();
     const dispatch = useDispatch();
-    console.log(productId);
+    console.log(product);
 
     const fetchProductDetail = async() => {
         const response = await axios.get(`https://fakestoreapi.com/products/${productId}`).catch(err => {
@@ -18,6 +18,9 @@ const ProductDetail = () => {
 
         dispatch(selectedProducts(response.data));
     }
+    useEffect(() => {
+        if(productId && productId !== "") fetchProductDetail();
+    },[productId])
     return (
         <div>
             <h1>ProductDetail</h1>
