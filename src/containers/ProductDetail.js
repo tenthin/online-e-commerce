eace389fd4b5f7facbe4935e6f790e983e23f015
Tenzin <tenthinten@gmail.com>
2022-01-20 21:15:49 +0530

    display product detail

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -7,6 +7,7 @@ import { useSelector } from 'react-redux';
 
 const ProductDetail = () => {
     const product = useSelector((state) => state.product);
+    const {image,title,price,category,description} = product
     const {productId} = useParams();
     const dispatch = useDispatch();
     console.log(product);
@@ -22,11 +23,37 @@ const ProductDetail = () => {
         if(productId && productId !== "") fetchProductDetail();
     },[productId])
     return (
-        <div>
-            <h1>ProductDetail</h1>
+        <div className="ui grid container">
+        {Object.keys(product).length === 0 ? (
+            <div>...Loading</div>
+        ) : (
+            <div className="ui placeholder segment">
+            <div className="ui two column stackable center aligned grid">
+                <div className="ui vertical divider">AND</div>
+                <div className="middle aligned row">
+                <div className="column lp">
+                    <img className="ui fluid image" src={image} />
+                </div>
+                <div className="column rp">
+                    <h1>{title}</h1>
+                    <h2>
+                    <a className="ui teal tag label">${price}</a>
+                    </h2>
+                    <h3 className="ui brown block header">{category}</h3>
+                    <p>{description}</p>
+                    <div className="ui vertical animated button" tabIndex="0">
+                    <div className="hidden content">
+                        <i className="shop icon"></i>
+                    </div>
+                    <div className="visible content">Add to Cart</div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        )}
         </div>
     )
-
 }
 
 export default ProductDetail;
