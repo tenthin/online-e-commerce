01f8a743873d52b6495659fcf28a047e2759e10c
Tenzin <tenthinten@gmail.com>
2022-01-20 12:56:04 +0530

    create redux files and actions

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,20 @@
+import { ActionTypes } from "../constants/action-types";
+
+const initialState = {
+    product: [
+        {
+        id:1,
+        title:"Tenzin",
+        category:"programmer",
+        }
+    ]
+}
+
+export const productReducer = (state, {type, payload}) => {
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return state;
+        default:
+            return state;
+    }
+}
