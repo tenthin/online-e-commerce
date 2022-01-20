e66b43964a4aaaf3a19508db0475022184aabcbf
Tenzin <tenthinten@gmail.com>
2022-01-20 21:06:57 +0530

    access to products

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -2,8 +2,11 @@ import React,{ useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
+import  { selectedProducts } from '../reduc/actions/productActions'
+import { useSelector } from 'react-redux';
 
 const ProductDetail = () => {
+    const product = useSelector((state) => state.product);
     const {productId} = useParams();
     const dispatch = useDispatch();
     console.log(productId);
@@ -13,7 +16,7 @@ const ProductDetail = () => {
             console.log('Err',err)
         })
 
-        dispatch();
+        dispatch(selectedProducts(response.data));
     }
     return (
         <div>
