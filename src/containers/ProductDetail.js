ef91b5bbc5c659f4ba3194ff2ed4c5005e2d7770
Tenzin <tenthinten@gmail.com>
2022-01-20 18:57:41 +0530

    link for single product

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
 
 const ProductDetail = () => {
+    const {productId} = useParams();
+    const dispatch = useDispatch();
+    console.log(productId);
+
+    const fetchProductDetail = async() => {
+        const response = await axios.get(`https://fakestoreapi.com/products/${productId}`).catch(err => {
+            console.log('Err',err)
+        })
+
+        dispatch();
+    }
     return (
         <div>
             <h1>ProductDetail</h1>
